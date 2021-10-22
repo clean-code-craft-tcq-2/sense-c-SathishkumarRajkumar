2404e218333f9eb355531df3f8d8b0259194492e
SathishkumarRajkumar <90903231+SathishkumarRajkumar@users.noreply.github.com>
2021-10-22 19:05:49 +0530

    Add files via upload

---
+++ alerters.c
@@ -0,0 +1,13 @@
+#include "alerters.h"
+
+int emailAlerter(void)
+{
+	emailAlertCallCount = 1;
+	return emailAlertCallCount;
+}
+
+int ledAlerter(void)
+{
+	ledAlertCallCount = 1;
+	retunr ledAlertCallCount;
+}
---
+++ alerters.h
@@ -0,0 +1,7 @@
+
+
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
+
+int emailAlerter(void);
+int ledAlerter(void);
