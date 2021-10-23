7ced8a436ab54fa81bf79ea7d71a3761828d154c
SathishkumarRajkumar <90903231+SathishkumarRajkumar@users.noreply.github.com>
2021-10-23 19:02:59 +0530

    Update alerters.h

--- alerters.h
+++ alerters.h
@@ -1,6 +1,6 @@
 
-extern int emailAlertCallCount = 0;
-extern int ledAlertCallCount = 0;
+int emailAlertCallCount = 0;
+int ledAlertCallCount = 0;
 
 void emailAlerter(void);
 void ledAlerter(void);
