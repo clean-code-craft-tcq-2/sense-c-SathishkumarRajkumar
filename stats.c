7cbd7c2ca2037f45c6ebe5e8d4e21477aa01b88c
SathishkumarRajkumar <90903231+SathishkumarRajkumar@users.noreply.github.com>
2021-10-23 21:17:11 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -43,7 +43,7 @@ void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stat
 {
 	if (maxThreshold > computedStats.max)
 	{
-		/*alerters[0];*/
-		/*alerters[1];*/
+		alerters[0];
+		alerters[1];
 	}
 }
