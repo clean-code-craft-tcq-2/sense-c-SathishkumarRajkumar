fa2c836d2a75b3a2105282b9294357a960b24088
SathishkumarRajkumar <90903231+SathishkumarRajkumar@users.noreply.github.com>
2021-10-24 13:28:42 +0530

    Update alerters.c

--- alerters.c
+++ alerters.c
@@ -5,10 +5,10 @@ int ledAlertCallCount;
 
 void emailAlerter(void)
 {
-	emailAlertCallCount = 1;
+	/*emailAlertCallCount = 1; */
 }
 
 void ledAlerter(void)
 {
-	ledAlertCallCount = 1;
+	/*ledAlertCallCount = 1;*/
 }
