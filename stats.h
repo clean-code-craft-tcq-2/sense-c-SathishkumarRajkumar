b3198621b5c13844cbf7b170016ba4f263f718bc
SathishkumarRajkumar <90903231+SathishkumarRajkumar@users.noreply.github.com>
2021-10-25 13:32:21 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -9,4 +9,4 @@ extern int ledAlertCallCount;
 struct Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr *alerters, struct Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
