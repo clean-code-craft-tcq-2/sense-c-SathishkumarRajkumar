aa9943a03fb8d2c4501e92b1f736519edd30d937
SathishkumarRajkumar <90903231+SathishkumarRajkumar@users.noreply.github.com>
2021-10-25 13:30:11 +0530

    Update stats.c

--- stats.c
+++ stats.c
@@ -41,11 +41,11 @@ struct Stats compute_statistics(const float* numberset, int setlength)
 	return s;
 }
 
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats)
+void check_and_alert(float maxThreshold, alerter_funcptr *alerters, struct Stats computedStats)
 {
 	printf("Function called");
 	if (maxThreshold < computedStats.max)
 	{
-		*alerters;
+		alerters;
 	}
 }
